d1fdfd8c1d7b80fbb589924e22af392a48a5f8cd
Kevin Le Perf <codlabtech@gmail.com>
2017-03-12 19:27:11 +0100

    fix typo

--- modules/eep/eep-d1-ff-00.js
+++ modules/eep/eep-d1-ff-00.js
@@ -3,7 +3,7 @@ module.exports=function(eep,data){
 	var begin = eep.split("-");
 
 	//24 bytes of data
-	if(begin[0] === "d1" && data.length = 24*2){
+	if(begin[0] === "d1" && data.length == 24*2){
 		return [{
 			type:"contactair",
 			serial:data.substring(0, 6*2),
